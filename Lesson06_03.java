f7e5a6b44281767a5ba3a939fee8116a62848b84
ErinaHasesgawa0422 <hasegawa.erina0422@gmail.com>
2024-06-19 16:41:16 +0900

    変数名のエラー

--- Lesson06_03.java
+++ Lesson06_03.java
@@ -4,10 +4,14 @@ public class Lesson06_03 {
         int var01 = 2;
         int var_01 = 3;
         int var$01 = 4;
+        // int var-01 = 5;
+        // int 01var  = 6;
 
         System.out.println(var);
         System.out.println(var01);
         System.out.println(var_01);
         System.out.println(var$01);
+        // System.out.println(var-01);
+        // System.out.println(01var);
     }
 }
