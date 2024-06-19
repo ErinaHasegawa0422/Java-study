a64db3ed359535629d198b586a2d43736daa8ab0
ErinaHasesgawa0422 <hasegawa.erina0422@gmail.com>
2024-06-19 15:56:22 +0900

    sort

--- Greeting.java
+++ Greeting.java
@@ -1,10 +1,10 @@
 
 class Greeting{
     public static void main(String args[]){
-        System.out.println("Good morning");
+        System.out.println("Good evening");
 
         System.out.println("Good afternoon");
 
-        System.out.println("Good evening");
+        System.out.println("Good morning");
     }
 }
